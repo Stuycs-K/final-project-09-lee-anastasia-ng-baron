91b7a9bfc83119a7caf2a9ed365601b007b83037
Anastasia Lee <alee51>
2024-05-24 14:35:06 -0400

    started writing sbox table in SubBytes

--- encrypt.java
+++ encrypt.java
@@ -6,9 +6,16 @@ public class encrypt {
             state.addColumn(input[4*i], input[4*i+1], input[4*i+2], input[4*i+3]);
         }
     }
+    public static void SubBytes() {
+        int[] sbox = {0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76};
+        System.out.println((byte)sbox[5]);
+        System.out.println(sbox.length);
+    }
     public static void main(String[] args) {
-        byte b = (byte)01000011;
+        byte b = (byte)0b01101011;
+
         System.out.println((int)b);
+        SubBytes();
 
     }
 }
