40465a3ef3584b8cf55f89bc93a647f16e292137
Anastasia Lee <alee51>
2024-05-28 14:35:45 -0400

    completed SubBytes

--- encrypt.java
+++ encrypt.java
@@ -23,12 +23,17 @@ public class encrypt {
                                 0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf,
                                 0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16};
     public static void SubBytes() {
-        System.out.println((byte)sbox[5]);
-        System.out.println(sbox.length);
+        for (int c = 0; c < 4; c++) {
+            for (int r = 0; r < 4; r++) {
+                byte b = state.get(c)[r];
+                state.get(c)[r] = (byte)sbox[(int)b];
+            }
+        }
+        //System.out.println((byte)sbox[5]);
+        //System.out.println(sbox.length);
     }
     public static void main(String[] args) {
         byte b = (byte)0b01101011;
-
         System.out.println((int)b);
         SubBytes();
 
