238c6439d2cbfa18df9fe8f98502e89ecf836628
Baron-Ng <baronng25@gmail.com>
2024-05-28 14:50:19 -0400

    testing username

--- encrypt.java
+++ encrypt.java
@@ -1,7 +1,6 @@
-import java.util.*;
 public class encrypt {
     static Matrix state = new Matrix();
-    public static void makeState(byte[] input) {
+    public static void makeState(byte[] input) { //
         for (int i = 0; i < 4; i++) {
             state.addColumn(input[4*i], input[4*i+1], input[4*i+2], input[4*i+3]);
         }
